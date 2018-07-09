74f1a427df71c7aa3f56477c16514cda0acfdb3e
Juan Rivera <juanriv45@gmail.com>
2018-07-09 01:36:25 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
-  def current_user(session_hash)
+  def self.current_user(session_hash)
     @user = User.find_by_id(session[:user_id])
   end
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     !!session[:user_id]
   end
 end
